c67ec64d60910c6275d5dc856709810bd6e7a7a6
collinw7 <dcwill03@gmail.com>
2023-02-22 23:21:36 -0500

    global and individual css files, slideshow organization

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,13 +2,20 @@ const menu = document.querySelector(".menubtn");
 const navList = document.querySelector(".openmenu");
 
 menu.addEventListener("click", () => {
-  if (menu.classList.contains("active")) {
-    menu.classList.remove("active");
+  if (!navList.classList.contains("close")) {
     menu.innerHTML = "menu";
     navList.classList.toggle("close");
   } else {
-    menu.classList.add("active");
     menu.innerHTML = "close";
     navList.classList.toggle("close");
+  }
+});
+
+window.addEventListener("click", (e) => {
+  if (!(e.target === menu || e.target === navList)) {
+    if (!navList.classList.contains("close")) {
+      navList.classList.toggle("close");
+      menu.innerHTML = "menu";
+    }
   }
 });
--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,1 +1,25 @@
-import $ from "jquery";
+$(function () {
+  $(".content-container").load("../slideshow/MBDProteins.html");
+});
+
+var pages = [
+  "../slideshow/MBDProteins.html",
+  "../slideshow/NuRD-recruitment.html",
+  "../slideshow/NuRD-disruption.html",
+];
+
+var currentPage = 0;
+
+$(".next").click(function () {
+  if (!(currentPage == 2)) {
+    currentPage++;
+    $(".content-container").load(pages[currentPage])
+  }
+});
+
+$(".prev").click(function () {
+    if (!(currentPage == 0)){
+        currentPage--;
+        $(".content-container").load(pages[currentPage]);
+    }
+});
