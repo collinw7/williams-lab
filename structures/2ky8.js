35b70c3e0f2c362077524aeefe699649496c3e93
collinw7 <dcwill03@gmail.com>
2023-03-02 18:00:23 -0500

    implemented jmol scripting to change default views if protein structures

--- structures/2ky8.js
+++ structures/2ky8.js
@@ -1,1 +1,4 @@
-jmolApplet(400, "load ../assets/2ky8.pdb; cartoon only; color molecule;");
+jmolApplet(
+  400,
+  "load ../assets/2ky8.pdb; cartoon only; select protein; color cyan; select dna; color group; reset; center {0.8304999999999989 -4.0215 28.8575}; rotate z -21.95; rotate y 136.43; rotate z -110.78; zoom 152.09;"
+);
--- structures/2l2l.js
+++ structures/2l2l.js
@@ -1,1 +1,4 @@
-jmolApplet(400, "load ../assets/2l2l.pdb; cartoon only; color molecule;");
+jmolApplet(
+  400,
+  "load ../assets/2l2l.pdb; cartoon only; select :a; color blueviolet; select :b; color cyan; reset; center {40.1495 18.933500000000002 4.483499999999999}; rotate z 151.83; rotate y 127.41; rotate z -91.02; zoom 124.46; translate x -10; translate y 10;"
+);
