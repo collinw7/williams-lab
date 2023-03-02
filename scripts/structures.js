65625119c3d817a526ef7dc3456c186e38d31b09
collinw7 <dcwill03@gmail.com>
2023-03-01 21:07:18 -0500

    jsmol slideshow W.I.P.

---
+++ scripts/structures.js
@@ -0,0 +1,38 @@
+$(function() {});
+
+var pages = [
+  "#cMBD2",
+  "#coiled-coil"
+];
+
+var currentPage = 0;
+
+$(".pageNum").text((currentPage + 1) + "/" + (pages.length));
+
+$(".next").click(function () {
+  if (!(currentPage == pages.length - 1)) {
+    $(pages[currentPage]).toggle("close");
+    currentPage++;
+    $(pages[currentPage]).toggle("close");
+    $(".pageNum").text((currentPage + 1) + "/" + (pages.length));
+  } else if (currentPage == pages.length - 1) {
+    $(pages[currentPage]).toggle("close");
+    currentPage = 0;
+    $(pages[currentPage]).toggle("close");
+    $(".pageNum").text((currentPage + 1) + "/" + (pages.length));
+  }
+});
+
+$(".prev").click(function () {
+  if (!(currentPage == 0)) {
+    $(pages[currentPage]).toggle("close");
+    currentPage--;
+    $(pages[currentPage]).toggle("close");
+    $(".pageNum").text((currentPage + 1) + "/" + (pages.length));
+  } else if (currentPage == 0) {
+    $(pages[currentPage]).toggle("close");
+    currentPage = pages.length - 1;
+    $(pages[currentPage]).toggle("close");
+    $(".pageNum").text((currentPage + 1) + "/" + (pages.length));
+  }
+});
